aef3a728ee5f461c42f2c8df2afcfe319f93f828
Antonio Zanardo <zanardo@gmail.com>
2018-08-05 15:34:45 -0300

    show final result for check

--- check.sh
+++ check.sh
@@ -2,12 +2,15 @@
 # This script checks if the necessary tooling is installing on your
 # machine before creating VMs.
 
+ALLGOOD=1
+
 check_installed() {
 	echo -n "checking if $1 is installed... "
 	if hash "$1" 2>/dev/null; then
 		echo "ok"
 	else
 		echo "not found!"
+		ALLGOOD=0
 	fi
 }
 
@@ -17,9 +20,17 @@ check_sudo() {
 		echo "ok"
 	else
 		echo "error!"
+		ALLGOOD=0
 	fi
 }
 
 check_installed "sudo"
 check_sudo
 check_installed "virt-install"
+
+if [ "$ALLGOOD" = "0" ]; then
+	echo "problems were found. please correct them!"
+	exit 1
+else
+	echo "dependencies are correct!"
+fi
