3d3b040035ffd99693a9b7d3c1f314ab8acd6732
Antonio Zanardo <zanardo@gmail.com>
2018-08-05 15:25:19 -0300

    initial check command

---
+++ check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This script checks if the necessary tooling is installing on your
+# machine before creating VMs.
+
+tput reset
+
+check() {
+	echo -n "checking if $1 is installed... "
+	if hash "$1" 2>/dev/null; then
+		echo "ok"
+	else
+		echo "not found!"
+	fi
+}
+
+check "virt-install"
